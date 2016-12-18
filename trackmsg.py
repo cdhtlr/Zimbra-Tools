e0b92e1c184cd267864ce796ee511ab45bcd1520
Sidik Hadi Kurniadi <sidikhadik@windowslive.com>
2016-12-19 00:30:03 +0700

    Add files via upload

--- trackmsg.py
+++ trackmsg.py
@@ -25,7 +25,7 @@ parser.add_argument("-t", "--recipient", help="Filter mail to")
 parser.add_argument("-r", "--relay_host", help="Filter mail relayed to host")
 parser.add_argument("-s", "--status", help="Filter mail status")
 parser.add_argument("-a", "--attachment", help="Filter mail attachment")
-parser.add_argument("-h", "--header", help="Filter mail subject/header")
+parser.add_argument("-he", "--header", help="Filter mail subject/header")
 parser.add_argument("-l", "--logfile", help="Logfile to search", default="/var/log/mail.log")
 args = parser.parse_args()
 
@@ -110,71 +110,76 @@ for line in f:
 			message_ids = id_regex.findall(line)
 			if message_ids:
 				message_id = message_ids[0].lstrip()
-				if message_id:
-					if not message_id in messages_dict:
-						messages_dict[message_id] = {}
-						messages_dict[message_id]['id'] = message_id
-						messages_dict[message_id]['server'] = None
-						messages_dict[message_id]['sender'] = None
-						messages_dict[message_id]['recipient'] = None
-						messages_dict[message_id]['relay'] = None
-						messages_dict[message_id]['status'] = None
-						messages_dict[message_id]['attachment'] = None
-						messages_dict[message_id]['header'] = None
-						messages_dict[message_id]['score'] = "N/A"
-					
-					id = id_regex.findall(line)
-					if id:
-						messages_dict[message_id]['id'] = cap(id[0], 12)
-						break
-					
-					client = client_regex.findall(line)
-					if client:
-						messages_dict[message_id]['server'] = wrap_always(client[0], 14)
-						break
-					
-					sender = from_regex.findall(line)
-					if sender:
-						messages_dict[message_id]['sender'] = cap(sender[0], 28)
-						break
-					
-					to = to_regex.findall(line)
-					if to:
-						messages_dict[message_id]['recipient'] = cap(to[0], 28)
-						break
+				message_id_regex = re.compile(".* " + message_id + "[:,] .*")
+				if not message_id_regex in search_tokens:
+					if args.server or (args.sender and not client_regex.match(line)) or ((args.recipient or args.relay_host) and (not from_regex.match(line)  and not client_regex.match(line))):
+						#print line
+						search_tokens.append(message_id_regex)
+			if message_id:
+				if not message_id in messages_dict:
+					messages_dict[message_id] = {}
+					messages_dict[message_id]['id'] = message_id
+					messages_dict[message_id]['server'] = None
+					messages_dict[message_id]['sender'] = None
+					messages_dict[message_id]['recipient'] = None
+					messages_dict[message_id]['relay'] = None
+					messages_dict[message_id]['status'] = None
+					messages_dict[message_id]['attachment'] = None
+					messages_dict[message_id]['header'] = None
+					messages_dict[message_id]['score'] = "N/A"
+				
+				id = id_regex.findall(line)
+				if id:
+					messages_dict[message_id]['id'] = cap(id[0], 12)
+					break
+				
+				client = client_regex.findall(line)
+				if client:
+					messages_dict[message_id]['server'] = wrap_always(client[0], 14)
+					break
+				
+				sender = from_regex.findall(line)
+				if sender:
+					messages_dict[message_id]['sender'] = cap(sender[0], 28)
+					break
+				
+				to = to_regex.findall(line)
+				if to:
+					messages_dict[message_id]['recipient'] = cap(to[0], 28)
+					break
 
-					relay = relay_regex.findall(line)
-					if relay:
-						messages_dict[message_id]['relay'] = wrap_always(relay[0], 16)
-						break
-						
-					attachment = attachment_regex.findall(line)
-					if attachment:
-						messages_dict[message_id]['attachment'] = wrap_always(attachment[0], 16)
-						break
-						
-					header = header_regex.findall(line)
-					if header:
-						messages_dict[message_id]['header'] = wrap_always(header[0], 16)
-						break
-						
-					score = score_regex.findall(line)
-					if score:
-						amavisstatus = amavisstatus_regex.findall(line)
-						if not amavisstatus:
-							amavisstatus = "N/A"
-						else:
-							amavisstatus = amavisstatus[0].replace(" ", "\n")
-						score = "%s\n%s" % (score[0], amavisstatus)
-						messages_dict[message_id]['score'] = wrap_always(score, 15).replace("\n\n", "\n")
-						break
+				relay = relay_regex.findall(line)
+				if relay:
+					messages_dict[message_id]['relay'] = wrap_always(relay[0], 16)
+					break
+					
+				attachment = attachment_regex.findall(line)
+				if attachment:
+					messages_dict[message_id]['attachment'] = wrap_always(attachment[0], 16)
+					break
+					
+				header = header_regex.findall(line)
+				if header:
+					messages_dict[message_id]['header'] = wrap_always(header[0], 16)
+					break
+					
+				score = score_regex.findall(line)
+				if score:
+					amavisstatus = amavisstatus_regex.findall(line)
+					if not amavisstatus:
+						amavisstatus = "N/A"
+					else:
+						amavisstatus = amavisstatus[0].replace(" ", "\n")
+					score = "%s\n%s" % (score[0], amavisstatus)
+					messages_dict[message_id]['score'] = wrap_always(score, 15).replace("\n\n", "\n")
+					break
 
-					status = status_regex.findall(line)
-					if status:
-						messages_dict[message_id]['status'] = status[0][0]
-						messages_dict[message_id]['status_extended'] = wrap_always(status[0][1], 24)
-						table.add_row ([messages_dict[message_id]['id'], messages_dict[message_id]['status'], messages_dict[message_id]['sender'], messages_dict[message_id]['recipient'], messages_dict[message_id]['header'], messages_dict[message_id]['attachment'], messages_dict[message_id]['score'], messages_dict[message_id]['relay'], messages_dict[message_id]['server'], messages_dict[message_id]['status_extended']])
-						break
+				status = status_regex.findall(line)
+				if status:
+					messages_dict[message_id]['status'] = status[0][0]
+					messages_dict[message_id]['status_extended'] = wrap_always(status[0][1], 24)
+					table.add_row ([messages_dict[message_id]['id'], messages_dict[message_id]['status'], messages_dict[message_id]['sender'], messages_dict[message_id]['recipient'], messages_dict[message_id]['header'], messages_dict[message_id]['attachment'], messages_dict[message_id]['score'], messages_dict[message_id]['relay'], messages_dict[message_id]['server'], messages_dict[message_id]['status_extended']])
+					break
 
 			break
 
