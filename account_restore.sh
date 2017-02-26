7abaff465e1890b7feae30ffad753f33b08c9234
Sidik Hadi Kurniadi <sidikhadik@windowslive.com>
2017-02-26 19:11:33 +0700

    Update account_restore.sh
    Bug fixing and simplifying restore process

--- account_restore.sh
+++ account_restore.sh
@@ -1,63 +1,69 @@
 #!/bin/bash
 DOMAINS="domains.txt"
 USERS="emails.txt"
-DISTRIBUTIONLIST="distributionlist.txt"
-USERPASS="userpass"
 USERDATA="userdata"
-########################## Mengembalikan (restore) semua domain ke server saat ini ##########################
-echo "======> 1/6. Mulai : Import semua domain <======"
-for i in $(cat $DOMAINS); do /opt/zimbra/bin/zmprov cd "$i" zimbraAuthMech zimbra; echo "$i -- finished"; done
-echo "======> 1/6. Selesai : Import semua domain <======"
+USERFILTER="userfilter"
+DISTRIBUTIONLIST="distributionlist.txt"
+DISTRIBUTIONLIST_MEMBERS="distributionlist_members"
 
-########################## Mengembalikan (restore) semua email ke server saat ini ##########################
-echo "======> 2/6. Mulai : Import email dan password <======"
+############################ Mengembalikan (restore) semua domain ke server saat ini ############################
+echo "======> 1/3. Mulai : Import daftar domain <======"
+for i in $(cat $DOMAINS)
+do
+	/opt/zimbra/bin/zmprov cd "$i" zimbraAuthMech zimbra
+	echo "$i -- domain imported"
+done
+echo "======> 1/3. Selesai : Import daftar domain <======"
+
+################## Mengembalikan (restore) semua email beserta mailboxnya ke server saat ini ###################
+echo "======> 2/3. Mulai : Import daftar email non-admin beserta mailboxnya <======"
 for i in $(cat $USERS)
 do
-givenName=$(grep givenName: $USERDATA/$i.txt | cut -d ":" -f2)
-displayName=$(grep displayName: $USERDATA/$i.txt | cut -d ":" -f2)
-shadowpass=$(cat $USERPASS/$i.shadow)
-tmpPass="CHANGEme"
-/opt/zimbra/bin/zmprov ca "$i" CHANGEme cn "$givenName" displayName "$displayName" givenName "$givenName" 
-/opt/zimbra/bin/zmprov ma "$i" userPassword "$shadowpass"
-echo "$i -- finished"
+	cn=$(grep cn: $USERDATA/$i.txt | cut -d ":" -f2)
+	givenName=$(grep givenName: $USERDATA/$i.txt | cut -d ":" -f2)
+	displayName=$(grep displayName: $USERDATA/$i.txt | cut -d ":" -f2)
+	zimbraAccountStatus=$(grep zimbraAccountStatus: $USERDATA/$i.txt | cut -d ":" -f2)
+	zimbraFilePreviewMaxSize=$(grep zimbraFilePreviewMaxSize: $USERDATA/$i.txt | cut -d ":" -f2)
+	zimbraFileUploadMaxSizePerFile=$(grep zimbraFileUploadMaxSizePerFile: $USERDATA/$i.txt | cut -d ":" -f2)
+	zimbraFeatureFiltersEnabled=$(grep zimbraFeatureFiltersEnabled: $USERDATA/$i.txt | cut -d ":" -f2)
+	zimbraFeatureSignaturesEnabled=$(grep zimbraFeatureSignaturesEnabled: $USERDATA/$i.txt | cut -d ":" -f2)
+	zimbraFeatureExportFolderEnabled=$(grep zimbraFeatureExportFolderEnabled: $USERDATA/$i.txt | cut -d ":" -f2)
+	zimbraFeatureImportFolderEnabled=$(grep zimbraFeatureImportFolderEnabled: $USERDATA/$i.txt | cut -d ":" -f2)
+	zimbraFeatureImportExportFolderEnabled=$(grep zimbraFeatureImportExportFolderEnabled: $USERDATA/$i.txt | cut -d ":" -f2)
+	zimbraMailForwardingAddress=$(grep zimbraMailForwardingAddress: $USERDATA/$i.txt | cut -d ":" -f2)
+	zimbraFeatureMailForwardingEnabled=$(grep zimbraFeatureMailForwardingEnabled: $USERDATA/$i.txt | cut -d ":" -f2)
+	zimbraFeatureMailForwardingInFiltersEnabled=$(grep zimbraFeatureMailForwardingInFiltersEnabled: $USERDATA/$i.txt | cut -d ":" -f2)
+	zimbraMailQuota=$(grep zimbraMailQuota: $USERDATA/$i.txt | cut -d ":" -f2)
+	zimbraMaxContactsPerPage=$(grep zimbraMaxContactsPerPage: $USERDATA/$i.txt | cut -d ":" -f2)
+	zimbraMaxMailItemsPerPage=$(grep zimbraMaxMailItemsPerPage: $USERDATA/$i.txt | cut -d ":" -f2)
+	zimbraPrefContactsPerPage=$(grep zimbraPrefContactsPerPage: $USERDATA/$i.txt | cut -d ":" -f2)
+	zimbraPrefMailForwardingAddress=$(grep zimbraPrefMailForwardingAddress: $USERDATA/$i.txt | cut -d ":" -f2)
+	zimbraPrefTimeZoneId=$(grep zimbraPrefInboxUnreadLifetime: $USERDATA/$i.txt | cut -d ":" -f2)
+	userPassword=$(grep userPassword: $USERDATA/$i.txt | cut -d ":" -f2)
+	tmpPass="CHANGEme"
+	/opt/zimbra/bin/zmprov ca "$i" "$tmpPass" cn "$cn" givenName "$givenName" displayName "$displayName" zimbraAccountStatus "$zimbraAccountStatus" zimbraFilePreviewMaxSize "$zimbraFilePreviewMaxSize" zimbraFileUploadMaxSizePerFile "$zimbraFileUploadMaxSizePerFile" zimbraFeatureFiltersEnabled "$zimbraFeatureFiltersEnabled" zimbraFeatureSignaturesEnabled "$zimbraFeatureSignaturesEnabled" zimbraFeatureExportFolderEnabled "$zimbraFeatureExportFolderEnabled" zimbraFeatureImportFolderEnabled "$zimbraFeatureImportFolderEnabled" zimbraFeatureImportExportFolderEnabled "$zimbraFeatureImportExportFolderEnabled" zimbraMailForwardingAddress="$zimbraMailForwardingAddress" zimbraFeatureMailForwardingEnabled "$zimbraFeatureMailForwardingEnabled" zimbraFeatureMailForwardingInFiltersEnabled "$zimbraFeatureMailForwardingInFiltersEnabled" zimbraMailQuota "$zimbraMailQuota" zimbraMaxContactsPerPage "$zimbraMaxContactsPerPage" zimbraMaxMailItemsPerPage "$zimbraMaxMailItemsPerPage" zimbraPrefContactsPerPage "$zimbraPrefContactsPerPage" zimbraPrefMailForwardingAddress "$zimbraPrefMailForwardingAddress" zimbraPrefTimeZoneId "$zimbraPrefTimeZoneId" zimbraFeatureChangePasswordEnabled "FALSE"
+	/opt/zimbra/bin/zmprov ma "$i" userPassword "$userPassword"
+	echo "$i -- user imported"
+	/opt/zimbra/bin/zmmailbox -z -m "$i" postRestURL "//?fmt=zip&resolve=reset" "$USERDATA"/"$i".zip
+	echo "$i -- data restored"
+	/opt/zimbra/bin/zmprov ma "$i" zimbraMailSieveScript "'$(cat $USERFILTER/$i.txt)'"
+	echo "$i -- filter restored"
 done
-echo "======> 2/6. Selesai : Import email dan password <======"
+echo "======> 2/3. Selesai : Import daftar email non-admin beserta mailboxnya <======"
 
-### Mengembalikan (restore) semua folder Mail, Contacts, Calendars, Briefcase, Tasks, Searches, Tags, dan Folders ###
-echo "======> 3/6. Mulai : Import daftar folder <======"
-for i in $(cat $USERS); do /opt/zimbra/bin/zmmailbox -z -m "$i" postRestURL "/?fmt=tgz&resolve=skip" "$i".tgz; echo "$i -- finished"; done
-echo "======> 3/6. Selesai : Import daftar folder <======"
-
-############################# Mengembalikan (restore) daftar distribution list ##############################
-echo "======> 4/6. Mulai : Import daftar distributionlist <======"
-for i in $(cat $DISTRIBUTIONLIST); do /opt/zimbra/bin/zmprov cdl "$i" ; echo "$i -- finished" ; done
-echo "======> 4/6. Selesai : Import daftar distributionlist <======"
-
-####################### Mengembalikan (restore) daftar email dari distribution list #########################
-echo "======> 5/6. Mulai : Import daftar email dari distribution list <======"
+##################### Mengembalikan (restore) daftar distribution list beserta membernya ######################
+echo "======> 3/3. Mulai : Import daftar distributionlist <======"
 for i in $(cat $DISTRIBUTIONLIST)
 do
-	for j in "grep -v '#' distributionlist_members/$i.txt |grep '@'" 
+	/opt/zimbra/bin/zmprov cdl "$i"
+	echo "$i -- distributionlist imported"
+	for j in "grep -v '#' $DISTRIBUTIONLIST_MEMBERS/$i.txt |grep '@'"
 	do
-	/opt/zimbra/bin/zmprov adlm "$i" "$j"
-	echo " $j member has been added to list $i"
+		/opt/zimbra/bin/zmprov adlm "$i" "$j"
+		echo "$j member has been added to list $i"
 	done
 done
-echo "======> 5/6. Selesai : Import daftar email dari distribution list <======"
+echo "======> 3/3. Selesai : Import daftar distributionlist <======"
 
-######################################### Import daftar email alias #########################################
-echo "======> 6/6. Mulai : Import semua email alias <======"
-for i in $(cat $USERS)
-do
-	if [ -f "alias/$i.txt" ]; then
-	for j in "grep '@' alias/$i.txt"
-	do
-	/opt/zimbra/bin/zmprov aaa "$i" "$j"
-	echo "$i HAS ALIAS $j --- Restored"
-	done
-	fi
-done
-echo "======> 6/6. Selesai : Import semua email alias <======"
-
-################################## Selesai bro, tinggal cek bener atw kaga ##################################
-echo "Selesai bro, tinggal cek bener atw kaga"
+################################### Selesai bro, tinggal cek bener atw kaga ###################################
+echo "Selesai bro, tinggal cek bener atw kaga :)"
