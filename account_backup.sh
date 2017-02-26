89f3f3ec5ac5c024eb50b5863645f2de9164c5ff
Sidik Hadi Kurniadi <sidikhadik@windowslive.com>
2017-02-26 18:42:34 +0700

    Update account_backup.sh
    Bug fixing and simplified backup process

--- account_backup.sh
+++ account_backup.sh
@@ -1,65 +1,48 @@
 #!/bin/bash
 DOMAINS="domains.txt"
-ADMINS="admins.txt"
 USERS="emails.txt"
-DISTRIBUTIONLIST="distributionlist.txt"
-USERPASS="userpass"
 USERDATA="userdata"
-############################# Export daftar domain ##############################
-echo "======> 1/9. Mulai : Export daftar domain <======"
+USERFILTER="userfilter"
+DISTRIBUTIONLIST="distributionlist.txt"
+DISTRIBUTIONLIST_MEMBERS="distributionlist_members"
+
+###################################### Export daftar domain #######################################
+echo "======> 1/3. Mulai : Export daftar domain <======"
 /opt/zimbra/bin/zmprov gad > "$DOMAINS"
 cat "$DOMAINS"
-echo "======> 1/9. Selesai : Export daftar domain <======"
+echo "======> 1/3. Selesai : Export daftar domain <======"
 
-############################## Export daftar semua email yang merupakan admin ###############################
-echo "======> 2/9. Mulai : Export daftar admin <======"
-/opt/zimbra/bin/zmprov gaaa > "$ADMINS"
-cat "$ADMINS"
-echo "======> 2/9. Selesai : Export daftar admin <======"
-
-################################ Export daftar semua email yang bukan admin #################################
-echo "======> 3/9. Mulai : Export daftar email non-admin <======"
+#################### Export daftar semua email beserta mailbox dan filter rule ####################
+echo "======> 2/3. Mulai : Export daftar email beserta mailbox dan filter rule <======"
 /opt/zimbra/bin/zmprov -l gaa > "$USERS"
 cat "$USERS"
-echo "======> 3/9. Selesai : Export daftar email non-admin <======"
+mkdir "$USERDATA"
+mkdir "$USERFILTER"
+for i in $(cat $USERS)
+    do
+        /opt/zimbra/bin/zmprov -l ga "$i" > "$USERDATA"/"$i".txt
+        echo "$i -- email exported"
+        /opt/zimbra/bin/zmmailbox -z -m "$i" getRestURL '//?fmt=zip' > "$USERDATA"/"$i".zip
+        echo "$i -- data exported"
+        /opt/zimbra/bin/zmprov ga "$i" zimbraMailSieveScript > "$USERFILTER"/"$i".txt
+        sed -i -e "1d" "$USERFILTER"/"$i".txt
+        sed 's/zimbraMailSieveScript: //g' "$USERFILTER"/"$i".txt > "$USERFILTER"/"$i".txt
+        echo "$i -- filter exported"
+    done
+echo "======> 2/3. Selesai : Export daftar email beserta mailbox dan filter rule <======"
 
-###################################### Export daftar distribution list ######################################
-echo "======> 4/9. Mulai : Export daftar distribution list <======"
+######################## Export daftar distribution list beserta membernya ########################
+echo "======> 3/3. Mulai : Export daftar distribution list beserta membernya <======"
 /opt/zimbra/bin/zmprov gadl > "$DISTRIBUTIONLIST"
 cat "$DISTRIBUTIONLIST"
-echo "======> 4/9. Selesai : Export daftar distribution list <======"
+mkdir "$DISTRIBUTIONLIST_MEMBERS"
+for i in $(cat $DISTRIBUTIONLIST)
+    do
+        /opt/zimbra/bin/zmprov gdlm "$i" > "$DISTRIBUTIONLIST_MEMBERS"/"$i".txt
+        echo "$i -- distributionlist_members exported"
+    done
+echo "======> 3/3. Selesai : Export daftar distribution list beserta membernya <======"
 
-############################# Export semua member dari daftar distribution list #############################
-echo "======> 5/9. Mulai : Export semua member dari daftar distribution list <======"
-mkdir distributionlist_members
-for i in $(cat $DISTRIBUTIONLIST); do /opt/zimbra/bin/zmprov gdlm "$i" > distributionlist_members/"$i".txt; echo "$i -- finished"; done
-echo "======> 5/9. Selesai : Export semua member dari daftar distribution list <======"
-
-############################# Export password dari semua email yang bukan admin #############################
-echo "======> 6/9. Mulai : Export semua password email non-admin <======"
-mkdir "$USERPASS"
-for i in $(cat $USERS); do /opt/zimbra/bin/zmprov -l ga "$i" userPassword | grep userPassword: | awk '{ print $2}' > "$USERPASS"/"$i".shadow; echo "$i -- finished"; done
-echo "======> 6/9. Selesai : Export semua password email non-admin <======"
-
-############## Export user name, display name, dan given name dari semua email yang bukan admin #############
-echo "======> 7/9. Mulai : Export user name, display name, dan given name dari semua email non-admin <======"
-mkdir "$USERDATA"
-for i in $(cat $USERS); do /opt/zimbra/bin/zmprov ga "$i" | grep -i Name: > "$USERDATA"/"$i".txt; echo "$i -- finished"; done
-echo "======> 7/9. Selesai : Export user name, display name, dan given name dari semua email non-admin <======"
-
-### Membuat backup berupa file arsip untuk masing-masing email yang bukan admin. Untuk nanti di-restore #####
-############# Terdiri dari Mail, Contacts, Calendars, Briefcase, Tasks, Searches, Tags, Folders #############
-########################################## Kecuali Junk dan Trash ###########################################
-echo "======> 8/9. Mulai : Mencadangkan folder Mail, Contacts, Calendars, Briefcase, Tasks, Searches, Tags, dan Folders dari semua email non-admin <======"
-for email in $(cat $USERS); do /opt/zimbra/bin/zmmailbox -z -m "$i" getRestURL '/?fmt=tgz' > "$i".tgz; echo "$email -- finished"; done
-echo "======> 8/9. Selesai : Mencadangkan folder Mail, Contacts, Calendars, Briefcase, Tasks, Searches, Tags, dan Folders dari semua email non-admin <======"
-
-######################################### Export daftar email alias #########################################
-echo "======> 9/9. Mulai : Export semua email alias <======"
-mkdir -p alias/
-for i in $(cat $USERS); do /opt/zimbra/bin/zmprov ga "$i" | grep zimbraMailAlias |awk '{print $2}' > alias/"$i".txt; echo "$i -- finished"; done
-find alias/ -type f -empty | xargs -n1 rm -v 
-echo "======> 9/9. Selesai : Export semua email alias <======"
-
-######## Selesai bro, tinggal pindahin semua file hasil backup ke server tujuan (baru/tempat restore) pake rsync atau manual boleh #######
-echo "Selesai bro, tinggal pindahin semua file hasil backup ke server tujuan (baru/tempat restore) pake rsync atau manual boleh"
+############################################ Selesai ##############################################
+echo "Selesai bro, tinggal pindah secara manual atau pakai rsync :)"
+echo "JANGAN LUPA HAPUS USER ADMIN, SPAM, HAM, dll yg ga penting :p"
