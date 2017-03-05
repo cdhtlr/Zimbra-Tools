7d1b3e9f958397da09f7c701c635adae6a5db09c
Sidik Hadi Kurniadi <sidikhadik@windowslive.com>
2017-03-05 21:23:20 +0700

    Update account_backup.sh

--- account_backup.sh
+++ account_backup.sh
@@ -2,7 +2,10 @@
 DOMAINS="domains.txt"
 USERS="emails.txt"
 USERDATA="userdata"
+USERALIAS="useralias"
 USERFILTER="userfilter"
+USERFORWARD="userforward"
+USERPREFEREDFORWARD="userpreferedforward"
 DISTRIBUTIONLIST="distributionlist.txt"
 DISTRIBUTIONLIST_MEMBERS="distributionlist_members"
 
@@ -12,24 +15,63 @@ echo "======> 1/3. Mulai : Export daftar domain <======"
 cat "$DOMAINS"
 echo "======> 1/3. Selesai : Export daftar domain <======"
 
-#################### Export daftar semua email beserta mailbox dan filter rule ####################
-echo "======> 2/3. Mulai : Export daftar email beserta mailbox dan filter rule <======"
+############################# Export daftar semua email dan seisinya ##############################
+echo "======> 2/3. Mulai : Export daftar email dan seisinya <======"
 /opt/zimbra/bin/zmprov -l gaa > "$USERS"
 cat "$USERS"
 mkdir "$USERDATA"
+mkdir "$USERALIAS"
 mkdir "$USERFILTER"
+mkdir "$USERFORWARD"
+mkdir "$USERPREFEREDFORWARD"
 for i in $(cat $USERS)
 do
 	/opt/zimbra/bin/zmprov -l ga "$i" > "$USERDATA"/"$i".txt
 	echo "$i -- email exported"
-	/opt/zimbra/bin/zmmailbox -z -m "$i" getRestURL '//?fmt=zip' > "$USERDATA"/"$i".zip
-	echo "$i -- data exported"
+	
+	/opt/zimbra/bin/zmprov ga "$i" zimbraMailAlias > "$USERALIAS"/"$i".txt
+	sed -i -e "1d" "$USERALIAS"/"$i".txt
+	sed -i -e "s/zimbraMailAlias: //g" "$USERALIAS"/"$i".txt
+	sed -i -e "s/^\t*$//g; s/^ *//; s/ *$//; /^$/d" "$USERALIAS"/"$i".txt
+	if [[ $(find $USERALIAS"/"$i".txt" -type f -size +0c 2>/dev/null) ]]; then
+		echo "$i -- alias exported"
+	fi
+
 	/opt/zimbra/bin/zmprov ga "$i" zimbraMailSieveScript > "$USERFILTER"/"$i".txt
 	sed -i -e "1d" "$USERFILTER"/"$i".txt
 	sed -i -e "s/zimbraMailSieveScript: //g" "$USERFILTER"/"$i".txt
-	echo "$i -- filter exported"
-done 
-echo "======> 2/3. Selesai : Export daftar email beserta mailbox dan filter rule <======"
+	sed -i -e "s/^\t*$//g; s/^ *//; s/ *$//; /^$/d" "$USERFILTER"/"$i".txt
+	if [[ $(find $USERFILTER"/"$i".txt" -type f -size +0c 2>/dev/null) ]]; then
+		echo "$i -- filter exported"
+	fi
+
+	/opt/zimbra/bin/zmprov ga "$i" zimbraMailForwardingAddress > "$USERFORWARD"/"$i".txt
+	sed -i -e "1d" "$USERFORWARD"/"$i".txt
+	sed -i -e "s/zimbraMailForwardingAddress: //g" "$USERFORWARD"/"$i".txt
+	sed -i -e "s/^\t*$//g; s/^ *//; s/ *$//; /^$/d" "$USERFORWARD"/"$i".txt
+	if [[ $(find $USERFORWARD"/"$i".txt" -type f -size +0c 2>/dev/null) ]]; then
+		echo "$i -- forwardlist exported"
+	fi
+
+	/opt/zimbra/bin/zmprov ga "$i" zimbraPrefMailForwardingAddress > "$USERPREFEREDFORWARD"/"$i".txt
+	sed -i -e "1d" "$USERPREFEREDFORWARD"/"$i".txt
+	sed -i -e "s/zimbraPrefMailForwardingAddress: //g" "$USERPREFEREDFORWARD"/"$i".txt
+	sed -i -e "s/^\t*$//g; s/^ *//; s/ *$//; /^$/d" "$USERPREFEREDFORWARD"/"$i".txt
+	if [[ $(find $USERPREFEREDFORWARD"/"$i".txt" -type f -size +0c 2>/dev/null) ]]; then
+		echo "$i -- prefered forwardlist exported"
+	fi
+
+	/opt/zimbra/bin/zmmailbox -z -m "$i" getRestURL '//?fmt=zip' > "$USERDATA"/"$i".zip
+	if [[ $(find $USERDATA"/"$i".zip" -type f -size +1c 2>/dev/null) ]]; then
+		echo "$i -- data exported"
+	fi
+done
+find "$USERDATA"/ -type f -size -2c | xargs -n1 rm -v
+find "$USERALIAS"/ -type f -empty | xargs -n1 rm -v
+find "$USERFILTER"/ -type f -empty | xargs -n1 rm -v
+find "$USERFORWARD"/ -type f -empty | xargs -n1 rm -v
+find "$USERPREFEREDFORWARD"/ -type f -empty | xargs -n1 rm -v
+echo "======> 2/3. Selesai : Export daftar email dan seisinya <======"
 
 ######################## Export daftar distribution list beserta membernya ########################
 echo "======> 3/3. Mulai : Export daftar distribution list beserta membernya <======"
