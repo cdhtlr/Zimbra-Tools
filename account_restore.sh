36f82f53b9d6fd39b6a47663fe2493472f1a99c6
Sidik Hadi Kurniadi <sidikhadik@windowslive.com>
2017-03-07 11:21:05 +0700

    Update account_restore.sh

--- account_restore.sh
+++ account_restore.sh
@@ -2,7 +2,10 @@
 DOMAINS="domains.txt"
 USERS="emails.txt"
 USERDATA="userdata"
+USERALIAS="useralias"
 USERFILTER="userfilter"
+USERFORWARD="userforward"
+USERPREFEREDFORWARD="userpreferedforward"
 DISTRIBUTIONLIST="distributionlist.txt"
 DISTRIBUTIONLIST_MEMBERS="distributionlist_members"
 
@@ -15,8 +18,8 @@ do
 done
 echo "======> 1/3. Selesai : Import daftar domain <======"
 
-################## Mengembalikan (restore) semua email beserta mailboxnya ke server saat ini ###################
-echo "======> 2/3. Mulai : Import daftar email non-admin beserta mailboxnya <======"
+###################### Mengembalikan (restore) semua email dan seisinya ke server saat ini ######################
+echo "======> 2/3. Mulai : Import daftar email dan seisinya <======"
 for i in $(cat $USERS)
 do
 	cn=$(grep cn: $USERDATA/$i.txt | cut -d ":" -f2)
@@ -25,33 +28,58 @@ do
 	zimbraAccountStatus=$(grep zimbraAccountStatus: $USERDATA/$i.txt | cut -d ":" -f2)
 	zimbraFilePreviewMaxSize=$(grep zimbraFilePreviewMaxSize: $USERDATA/$i.txt | cut -d ":" -f2)
 	zimbraFileUploadMaxSizePerFile=$(grep zimbraFileUploadMaxSizePerFile: $USERDATA/$i.txt | cut -d ":" -f2)
-	zimbraFeatureFiltersEnabled=$(grep zimbraFeatureFiltersEnabled: $USERDATA/$i.txt | cut -d ":" -f2)
 	zimbraFeatureSignaturesEnabled=$(grep zimbraFeatureSignaturesEnabled: $USERDATA/$i.txt | cut -d ":" -f2)
 	zimbraFeatureExportFolderEnabled=$(grep zimbraFeatureExportFolderEnabled: $USERDATA/$i.txt | cut -d ":" -f2)
 	zimbraFeatureImportFolderEnabled=$(grep zimbraFeatureImportFolderEnabled: $USERDATA/$i.txt | cut -d ":" -f2)
 	zimbraFeatureImportExportFolderEnabled=$(grep zimbraFeatureImportExportFolderEnabled: $USERDATA/$i.txt | cut -d ":" -f2)
-	zimbraMailForwardingAddress=$(grep zimbraMailForwardingAddress: $USERDATA/$i.txt | cut -d ":" -f2)
 	zimbraFeatureMailForwardingEnabled=$(grep zimbraFeatureMailForwardingEnabled: $USERDATA/$i.txt | cut -d ":" -f2)
+	zimbraPrefMailLocalDeliveryDisabled=$(grep zimbraPrefMailLocalDeliveryDisabled: $USERDATA/$i.txt | cut -d ":" -f2)
+	zimbraPrefContactsPerPage=$(grep zimbraPrefContactsPerPage: $USERDATA/$i.txt | cut -d ":" -f2)
+	zimbraPrefTimeZoneId=$(grep zimbraPrefTimeZoneId: $USERDATA/$i.txt | cut -d ":" -f2)
 	zimbraMailQuota=$(grep zimbraMailQuota: $USERDATA/$i.txt | cut -d ":" -f2)
 	zimbraMaxContactsPerPage=$(grep zimbraMaxContactsPerPage: $USERDATA/$i.txt | cut -d ":" -f2)
 	zimbraMaxMailItemsPerPage=$(grep zimbraMaxMailItemsPerPage: $USERDATA/$i.txt | cut -d ":" -f2)
-	zimbraPrefContactsPerPage=$(grep zimbraPrefContactsPerPage: $USERDATA/$i.txt | cut -d ":" -f2)
-	zimbraPrefMailForwardingAddress=$(grep zimbraPrefMailForwardingAddress: $USERDATA/$i.txt | cut -d ":" -f2)
-	zimbraPrefTimeZoneId=$(grep zimbraPrefInboxUnreadLifetime: $USERDATA/$i.txt | cut -d ":" -f2)
 	userPassword=$(grep userPassword: $USERDATA/$i.txt | cut -d ":" -f2)
 	tmpPass="CHANGEme"
-	/opt/zimbra/bin/zmprov ca "$i" "$tmpPass" cn "$cn" givenName "$givenName" displayName "$displayName" zimbraAccountStatus "$zimbraAccountStatus" zimbraFilePreviewMaxSize "$zimbraFilePreviewMaxSize" zimbraFileUploadMaxSizePerFile "$zimbraFileUploadMaxSizePerFile" zimbraFeatureFiltersEnabled "$zimbraFeatureFiltersEnabled" zimbraFeatureSignaturesEnabled "$zimbraFeatureSignaturesEnabled" zimbraFeatureExportFolderEnabled "$zimbraFeatureExportFolderEnabled" zimbraFeatureImportFolderEnabled "$zimbraFeatureImportFolderEnabled" zimbraFeatureImportExportFolderEnabled "$zimbraFeatureImportExportFolderEnabled" zimbraMailForwardingAddress="$zimbraMailForwardingAddress" zimbraFeatureMailForwardingEnabled "$zimbraFeatureMailForwardingEnabled" zimbraFeatureMailForwardingInFiltersEnabled "$zimbraFeatureMailForwardingInFiltersEnabled" zimbraMailQuota "$zimbraMailQuota" zimbraMaxContactsPerPage "$zimbraMaxContactsPerPage" zimbraMaxMailItemsPerPage "$zimbraMaxMailItemsPerPage" zimbraPrefContactsPerPage "$zimbraPrefContactsPerPage" zimbraPrefMailForwardingAddress "$zimbraPrefMailForwardingAddress" zimbraPrefTimeZoneId "$zimbraPrefTimeZoneId" zimbraFeatureChangePasswordEnabled "FALSE" zimbraFeatureMailForwardingInFiltersEnabled "TRUE"
+	/opt/zimbra/bin/zmprov ca "$i" "$tmpPass" cn "$cn" givenName "$givenName" displayName "$displayName" zimbraAccountStatus "$zimbraAccountStatus" zimbraFilePreviewMaxSize "$zimbraFilePreviewMaxSize" zimbraFileUploadMaxSizePerFile "$zimbraFileUploadMaxSizePerFile" zimbraFeatureSignaturesEnabled "$zimbraFeatureSignaturesEnabled" zimbraFeatureExportFolderEnabled "$zimbraFeatureExportFolderEnabled" zimbraFeatureImportFolderEnabled "$zimbraFeatureImportFolderEnabled" zimbraFeatureImportExportFolderEnabled "$zimbraFeatureImportExportFolderEnabled" zimbraFeatureMailForwardingEnabled "$zimbraFeatureMailForwardingEnabled" zimbraPrefMailLocalDeliveryDisabled "$zimbraPrefMailLocalDeliveryDisabled" zimbraPrefContactsPerPage "$zimbraPrefContactsPerPage" zimbraPrefTimeZoneId "$zimbraPrefTimeZoneId" zimbraMailQuota "$zimbraMailQuota" zimbraMaxContactsPerPage "$zimbraMaxContactsPerPage" zimbraMaxMailItemsPerPage "$zimbraMaxMailItemsPerPage" zimbraFeatureChangePasswordEnabled "FALSE" zimbraFeatureFiltersEnabled "TRUE" zimbraFeatureMailForwardingInFiltersEnabled "TRUE"
 	/opt/zimbra/bin/zmprov ma "$i" userPassword "$userPassword"
-	echo "$i -- user imported"
-	/opt/zimbra/bin/zmmailbox -z -m "$i" postRestURL "//?fmt=zip&resolve=reset" "$USERDATA"/"$i".zip
-	echo "$i -- data restored"
-	/opt/zimbra/bin/zmprov ma "$i" zimbraMailSieveScript "'"$(cat $USERFILTER/$i.txt)"'"
-	echo "$i -- filter restored"
+	echo "$i -- email imported"
+	
+	if [[ $(-f $USERALIAS"/"$i".txt") ]]; then
+		for j in $(cat $USERALIAS"/"$i".txt")
+		do
+			/opt/zimbra/bin/zmprov aaa "$i" "$j"
+			echo "$i has alias $j -- Restored"
+		done 
+	fi
+	
+	if [[ $(-f $USERFILTER"/"$i".txt") ]]; then
+		/opt/zimbra/bin/zmprov ma "$i" zimbraMailSieveScript "'"$(cat $USERFILTER/$i.txt)"'"
+		echo "$i -- filter restored"
+	fi
+	
+	if [[ $(-f $USERFORWARD"/"$i".txt") ]]; then
+		for j in $(cat $USERFORWARD"/"$i".txt")
+		do
+			/opt/zimbra/bin/zmprov ma "$i" +zimbraMailForwardingAddress "$j"
+			echo "$i has forwarding address $j -- Restored"
+		done 
+	fi
+	
+	if [[ $(-f $USERPREFEREDFORWARD"/"$i".txt") ]]; then
+		/opt/zimbra/bin/zmprov ma "$i" zimbraPrefMailForwardingAddress $(cat $USERPREFEREDFORWARD/$i.txt)
+		echo "$i -- prefered forwarding address restored"
+	fi
+	
+	if [[ $(-f $USERDATA"/"$i".zip") ]]; then
+		/opt/zimbra/bin/zmmailbox -z -m "$i" postRestURL "//?fmt=zip&resolve=reset" "$USERDATA"/"$i".zip
+		echo "$i -- data restored"
+	fi	
 done
-echo "======> 2/3. Selesai : Import daftar email non-admin beserta mailboxnya <======"
+echo "======> 2/3. Selesai : Import daftar email dan seisinya <======"
 
 ##################### Mengembalikan (restore) daftar distribution list beserta membernya ######################
-echo "======> 3/3. Mulai : Import daftar distributionlist <======"
+echo "======> 3/3. Mulai : Import daftar distribution list beserta membernya <======"
 for i in $(cat $DISTRIBUTIONLIST)
 do
 	/opt/zimbra/bin/zmprov cdl "$i"
