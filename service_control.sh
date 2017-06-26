44de3ede6649b15cb435ee2582968456d1e24762
Sidik Hadi Kurniadi <sidikhadik@windowslive.com>
2017-06-27 04:29:29 +0700

    Update service_control.sh

--- service_control.sh
+++ service_control.sh
@@ -21,10 +21,14 @@ then
 	#Kirimkan e-mail berisi status service zimbra (harus menginstall aplikasi sendemail, bukan sendmail)
 	sendEmail -f "$from" -t "$target" -s "$smtp" -o tls="$tls" -xu "$from" -xp "$pass" -u "$subject" -m "$zimbra_service_status"
 	
-	#Stop service zimbra
-	su - zimbra -c 'zmcontrol stop'
+	#Matikan proses berdasarkan port yang digunakan, kemungkinan zimbra gagal dijalankan menggunakan port ini
+	ZimbraSafePorts=( 25, 53, 110, 143, 389, 443, 465, 587, 636, 993, 995, 3310, 3443, 7025, 7026, 7047, 7071, 7072, 7073, 7110, 7143, 7171, 7306, 7780, 7993, 7995, 8080, 8443, 8465, 9071, 10024, 10025, 10026, 10027, 10028, 10029, 10030, 10031, 10032, 10663, 11211, 23232, 23233)
+	for port in "${ZimbraSafePorts[@]}"
+	do
+		pid=$(lsof -i:$port -t); kill -TERM $pid || kill -KILL $pid
+	done
 	
-	#Restart OS
-	reboot
+	#Restart service zimbra
+	su - zimbra -c 'zmcontrol restart'
 
 fi
