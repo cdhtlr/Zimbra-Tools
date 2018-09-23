8cc17297eb10e1b3a6c8ef6ad6fc6104fcb95eb2
Sidik Hadi Kurniadi <sidikhadik@windowslive.com>
2018-09-24 04:38:26 +0700

    Update force_resend.sh

--- force_resend.sh
+++ force_resend.sh
@@ -2,7 +2,7 @@
 
 while true
 do
-	for EMAIL in $(/opt/zimbra/common/sbin/postqueue -p | grep -v ^- | grep -v '(' | cut -d ' ' -f11 | grep -e [[:alnum:]] | sort -u)
+	for EMAIL in $(/opt/zimbra/common/sbin/postqueue -p | grep -v ^- | grep -v '(' | cut -d ' ' -f10 | grep -e [[:alnum:]] | sort -u)
 	do
 		for ID in $(/opt/zimbra/common/sbin/postqueue -p | grep -v ^- | grep -v '(' | grep '$EMAIL' | cut -d ' ' -f1 | grep -e [[:alnum:]] | grep -v '[*!]$' | grep -m1 '')
 		do
